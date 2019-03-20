d91d21e99993c08e917e70a1e37441e18200e886
kdixon11 <48635137+kdixon11@users.noreply.github.com>
2019-03-20 13:01:46 -0400

    Add files via upload

---
+++ sumsquare.java
@@ -0,0 +1,12 @@
+public class sumsquare {
+	public static void main(String [] args){
+		int x = 3;
+		int sum = 0;
+		while (x <= 33){
+			sum += Math.pow(x , 2);
+		    System.out.printf("Cumulation results: %-8d%8s%8d%8s%8d\n", sum ,":", x ,"*", x );
+		    x += 3;
+		}
+		System.out.print("The SUM of Square of muiples of 3 is " + sum);
+	}	  
+}
